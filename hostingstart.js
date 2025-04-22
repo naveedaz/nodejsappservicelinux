2193a047225bef2caddbd54f41288e02064dc35b
Naveed Aziz <naveeda@microsoft.com>
2025-04-21 17:23:02 -0700

    Trying autoswap

--- hostingstart.js
+++ hostingstart.js
@@ -28,5 +28,5 @@ wss.on('connection', function(ws) {
 });
  
 server.listen(process.env.PORT, function() {
-    console.log('listening on ');
+    console.log('listening on port' process.env.PORT);
 });
